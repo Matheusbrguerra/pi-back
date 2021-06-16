13ec1627b865e59154bc827ed0b0a49ff792198b
Matheus Bromonschenkel Guerra <matheus.guerra@squadra.com.br>
2021-06-16 18:42:34 -0300

    Modificando o docker compose e a view inicial

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Utilize a url base nas rotas a seguir \n baseurl/auth/signup \n baseurl/auth/signin';
   }
 }
